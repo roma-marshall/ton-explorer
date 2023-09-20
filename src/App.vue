62f12c5d7cc9c2434d2bf6950e3e52eeb149e6dc
roma_marshall <roma_marshall@outlook.com>
2023-09-20 12:18:06 +0200

    init fetch api data

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <template>
-  <div></div>
+  <div class="flex justify-center">
+    <input type="text" v-model="id" @keypress.enter="runScan(id)" class="w-1/2 m-10 p-3 border-2 outline-none">
+  </div>
+  <div class="flex justify-center">
+    <ul v-if="result">
+      <li v-for="item in result['traces']">
+        {{ item }}
+      </li>
+      <li>
+        {{ result['error'] }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script setup>
+import { ref } from "vue"
 
+let result = ref(null)
+let id = ref()
+let traces = ref()
+let limit = 100
+
+const runScan = (id) => {
+  traces = `https://tonapi.io/v2/accounts/${id}/traces?limit=${limit}`
+  fetch(traces)
+      .then(response => response.json())
+      .then(data => result.value = data)
+}
 </script>
