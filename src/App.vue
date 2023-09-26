540063e7b93c8f9700d50c16218813e1ebb740a9
roma_marshall <roma_marshall@outlook.com>
2023-09-26 19:25:24 +0200

    transactions refactoring

--- src/App.vue
+++ src/App.vue
@@ -4,29 +4,52 @@
            class="w-1/2 m-10 p-3 border-2 outline-none"
            placeholder="Search TON addresses, domains and transactions...">
   </div>
-  <div class="flex justify-center">
-    <ul v-if="result">
-      <li v-for="item in result['traces']">
-        {{ item }}
-      </li>
-      <li>
+  <div class="relative overflow-x-auto">
+    <table v-if="result" class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead v-if="!result['error']" class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <tr>
+        <th scope="col" class="px-6 py-3">
+          utime
+        </th>
+        <th scope="col" class="px-6 py-3">
+          hash
+        </th>
+        <th scope="col" class="px-6 py-3">
+          address
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in result['transactions']" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+          {{ item['utime'] }}
+        </th>
+        <td class="px-6 py-4">
+          {{ item['hash'] }}
+        </td>
+        <td class="px-6 py-4">
+          {{ item['account']['address'] }}
+        </td>
+      </tr>
+      </tbody>
+      <p v-if="result['error']" class="flex justify-center">
         {{ result['error'] }}
-      </li>
-    </ul>
+      </p>
+    </table>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue"
+import { ref } from 'vue'
 
 let result = ref(null)
 let id = ref()
-let traces = ref()
-let limit = 100
+let transaction = ref()
+let limit = 50
 
 const runScan = (id) => {
-  traces = `https://tonapi.io/v2/accounts/${id}/traces?limit=${limit}`
-  fetch(traces)
+  transaction = `https://tonapi.io/v2/blockchain/accounts/${id}/transactions?limit=${limit}`
+  fetch(transaction)
       .then(response => response.json())
       .then(data => result.value = data)
 }
