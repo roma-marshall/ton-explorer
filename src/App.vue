90e69696cc5a3c443b34a6646b691d3f0d578abc
vires in numeris <79694559+roma-marshall@users.noreply.github.com>
2023-09-27 15:06:38 +0200

    rename utime in timestamp

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
       <thead v-if="!result['error']" class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
       <tr>
         <th scope="col" class="px-6 py-3">
-          utime
+          timestamp
         </th>
         <th scope="col" class="px-6 py-3">
           hash
