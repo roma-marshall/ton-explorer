1d92fcfb250bd83c8762510b38d00d3af5be12d7
roma_marshall <roma_marshall@outlook.com>
2023-10-12 15:24:26 +0200

    change API provider

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,49 @@
 <template>
   <div class="flex justify-center">
-    <input type="search" v-model="id" @keypress.enter="fetchData()"
+    <input type="search" v-model="ton" @keypress.enter="fetchData()"
            class="w-1/2 m-10 p-3 border-2 outline-none"
            placeholder="Search TON addresses, domains and transactions...">
   </div>
   <div class="relative overflow-x-auto">
     <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead v-if="data && !error" class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+      <thead v-if="isOk" class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
       <tr>
         <th scope="col" class="px-6 py-3">
           timestamp
         </th>
         <th scope="col" class="px-6 py-3">
-          hash
+          recipient
         </th>
         <th scope="col" class="px-6 py-3">
-          address
+          sender
+        </th>
+        <th scope="col" class="px-6 py-3">
+          amount
+        </th>
+        <th scope="col" class="px-6 py-3">
+          status
         </th>
       </tr>
       </thead>
       <tbody>
-      <tr v-if="data" v-for="(item, i) in data['transactions']" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+      <tr v-if="dataBeta" v-for="(item, i) in dataBeta['result']" class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
         <td class="px-6 py-4 whitespace-nowrap">
           {{ timestamp[i] }}
         </td>
         <td class="px-6 py-4">
-          {{ hash[i] }}
+          {{ recipient[i] }}
         </td>
         <td class="px-6 py-4">
-          {{ address[i] }}
+          {{ sender[i] }}
+        </td>
+        <td class="px-6 py-4">
+          {{ amount[i] }}
+        </td>
+        <td class="px-6 py-4">
+          {{ status[i] }}
         </td>
       </tr>
       </tbody>
-      <p v-if="error" class="flex justify-center">
-        {{ error }}
-      </p>
     </table>
   </div>
 </template>
@@ -42,37 +51,59 @@
 <script setup>
 import { ref } from 'vue'
 
-let data = ref()
-let id = null
+// let data = ref()
+let ton = null
 let limit = 50
+let dataBeta = ref()
 let timestamp = ref([])
-let hash = ref([])
-let address = ref([])
-let error = ref(null)
+let recipient = ref([])
+let sender = ref([])
+let amount = ref([])
+let status = ref([])
+let isOk = ref(null)
 
 const fetchData = async () => {
-  let url = `https://tonapi.io/v2/blockchain/accounts/${id}/transactions?limit=${limit}`
-  const response = await fetch(url)
-  data = await response.json()
-  error.value = data['error']
-  await getData(data)
+  const urlAlfa = `https://tonapi.io/v2/blockchain/accounts/${ton}/transactions?limit=${limit}`
+  const responseAlfa = await fetch(urlAlfa)
+  const dataAlfa = await responseAlfa.json()
+  const accountAddress = dataAlfa['transactions'][0]['account']['address']
+
+  const urlBeta = `https://toncenter.com/api/v2/getTransactions?address=${accountAddress}&limit=${limit}&to_lt=0&archival=false`
+  const responseBeta = await fetch(urlBeta)
+  dataBeta = await responseBeta.json()
+
+  isOk.value = dataBeta['ok']
+  await getData(dataBeta)
 }
 
-const getData = async (data) => {
-    data['transactions'].forEach(item => {
-      // timestamp
-      let timestampTemp = new Date(item['utime'] * 1000)
-      let month = timestampTemp.toLocaleString('en', { month: 'short' })
-      let day = timestampTemp.getDate()
-      let hours = timestampTemp.getHours()
-      let minutes = (timestampTemp.getMinutes() < 10 ? '0' : '') + timestampTemp.getMinutes()
-      timestamp.value.push(`${day} ${month}, ${hours}:${minutes}`)
+const getData = async (dataBeta) => {
+  dataBeta['result'].forEach(item => {
+    // timestamp
+    let timestampTemp = new Date(item['utime'] * 1000)
+    let month = timestampTemp.toLocaleString('en', { month: 'short' })
+    let day = timestampTemp.getDate()
+    let hours = timestampTemp.getHours()
+    let minutes = (timestampTemp.getMinutes() < 10 ? '0' : '') + timestampTemp.getMinutes()
+    timestamp.value.push(`${day} ${month}, ${hours}:${minutes}`)
 
-      // hash
-      hash.value.push(item['hash'])
-
-      // address
-      address.value.push(item['account']['address'])
-    })
+    // recipient, sender, amount, status
+    if (item['in_msg']['source'] === '') {
+      let recipientTemp = item['out_msgs'][0]['destination']
+      let senderTemp = item['out_msgs'][0]['source']
+      let amountTemp = item['out_msgs'][0]['value'] / 1000000000
+      recipient.value.push(recipientTemp)
+      sender.value.push(senderTemp)
+      amount.value.push(amountTemp)
+      status.value.push('in')
+    } else if (item['in_msg']['source'] !== '') {
+      let recipientTemp = item['in_msg']['destination']
+      let senderTemp = item['in_msg']['source']
+      let amountTemp = item['in_msg']['value'] / 1000000000
+      recipient.value.push(recipientTemp)
+      sender.value.push(senderTemp)
+      amount.value.push(amountTemp)
+      status.value.push('out')
+    }
+  })
 }
 </script>
